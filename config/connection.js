0a3d586fcfaa77f3509419ebb7d7296c0a68d7d7
Bryant Vail <Bryanttv@outlook.com>
2018-07-04 02:05:26 -0400

    const mongooseConnection        = require("./config/connection");

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,13 @@
 const mongoose      = require("mongoose");
 
-mongoose.connect("mongodb://localhost/shirtOrderLocal");
-//if directory used does not exist, the directory will be created to be used
-mongoose.connection.once("open", function(){
-    //.once == eventListener
-    console.log("Connection mongodb, 'mongoose.connection', done");
-}).on("error", function(error){
-       console.log(`Connection error: ${error.message}`);
-});
+module.exports = (function(){
+    mongoose.connect("mongodb://localhost/shirtOrderLocal");
+    //if directory used does not exist, the directory will be created to be used
+
+    mongoose.connection.once("open", function(){
+        //.once == eventListener
+        console.log("Connection mongodb, 'mongoose.connection', done");
+    }).on("error", function(error){
+        console.log(`Connection error: ${error.message}`);
+    });
+})();
